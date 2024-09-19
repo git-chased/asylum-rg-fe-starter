93e446066e4b0d2a75f6315e6861519950db2e29
Chase Darnall <emailchased@gmail.com>
2024-09-19 00:37:36 -0500

    first ticket wip

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,7 +32,20 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section"> 
+        <div className="graph-container">
+          <img src={GrantRatesByOfficeImg} alt='Grant Rates By Office Graph'/>
+          <h3>Search Grant Rates By Office</h3>
+        </div>
+        <div className="graph-container-mid">
+          <img src={GrantRatesByNationalityImg} alt='Grant Rates By Nationality'/>
+          <h3>Search Grant Rates By Nationality</h3>
+        </div>
+        <div className="graph-container">
+          <img src={GrantRatesOverTimeImg} alt='Grant Rates Over Time Graph'/>
+          <h3>Search Grant Rates Over Time</h3>
+        </div>
+      </div>
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -41,7 +54,15 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => console.log('Download Button Clicked')}
+        >
+          Download the Data
+        </Button>
       </div>
+      
 
       <div className="middle-section">
         <div className="hrf-img-container">
@@ -61,10 +82,46 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
-        <p onClick={() => scrollToTop()} className="back-to-top">
-          Back To Top ^
-        </p>
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div className='data-container'>
+            <div className='data-point-container'>
+              <div className='data-point'>36%</div>
+              <div className='data-details'>
+                By the end of the Trump administration, the average asylumn
+                office grant rate had fallen 36 percent from the average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </div>
+            </div>
+            <div className='data-point-container'>
+              <div className='data-point'>5%</div>
+              <div className='data-details'>
+                The New Your asylumn office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </div>
+            </div>
+            <div className='data-point-container'>
+              <div className='data-point'>6x Lower</div>
+              <div className='data-details'>
+                Between fiscal year 2017 and 2020, the New York asylumn office's
+                average grant rate was six times lower than the San Francisco
+                asylumn office.
+              </div>
+            </div>
+          </div>
+          <div className="view-more-data-btn-container">
+            <Button
+              type="default"
+              style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+              onClick={() => window.open('https://humanrightsfirst.org/library/uscis-records-reveal-systemic-disparities-in-asylum-decisions/', '_blank')}
+            >
+              Read More
+            </Button>
+          </div>
+          <p onClick={() => scrollToTop()} className="back-to-top">
+            Back To Top ^
+          </p>
+        </div>
       </div>
     </div>
   );
